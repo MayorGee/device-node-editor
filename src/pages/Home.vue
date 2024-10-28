93b57f832f84120e60c1c78397ea668feb8997af
mayorCoder <adebayomayowa78@gmail.com>
2024-10-28 17:20:05 +0300

    first commit - fresh vue project

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <h1 class="page__header">Page Header</h1>    
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.page {
+  color: green
+}
+</style>
